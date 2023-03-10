1b595416bb36108ecfe2bdf6782a75ad98524ca7
amanda jones <amandaoliveirajones@gmail.com>
2023-03-10 08:05:21 -0300

    finalizacao do tratamento dos dados e envio para banco de dados

--- code/connection.py
+++ code/connection.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
-import pyodbc
 import pandas as pd
+import sqlalchemy
+import glob
+
 
 load_dotenv()
 
@@ -9,24 +11,29 @@ server = os.environ['SERVER']
 database = os.environ['DATABASE']
 username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
+driver = 'ODBC Driver 18 for SQL Server'
+cnx_str = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
 
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+engine = sqlalchemy.create_engine(cnx_str)
 
-cursor.execute('SELECT * FROM Clientes')
-cursor.commit()
+path_clients = './data/clients/clients_clean'
+path_transaction = './data/transaction/transaction_clean'
+clients = glob.glob(path_clients + '/*.csv')
+transaction = glob.glob(path_transaction + '/*.csv')
 
-clean_clients = './data/clients/clients_clean/part-00000-dbc09eef-47f3-4e57-995d-6cc36d88e53f-c000.csv'
-df_clean_clients = pd.read_csv(clean_clients)
-print(df_clean_clients)
+df_clients = pd.read_csv(clients[0])
+df_transaction = pd.read_csv(transaction[0])
 
-# Definir uma função para inserir uma pessoa na tabela do SQL Server
-#def inserir_pessoa(pessoa):
-    # cursor.execute("INSERT INTO Tabela de clientes (Id, Nome, Email, Data_cadastro, Telefone, DDD, Country_code) VALUES (?, ?)", pessoa.Nome, pessoa.Idade)
-    # cursor.commit()
+df_clients['Data_cadastro'] = pd.to_datetime(df_clients['Data_cadastro'], format='%Y-%m-%d %H:%M:%S')
+df_clients['Data_cadastro'] = df_clients['Data_cadastro'].dt.tz_localize(None)
 
-#for index, row in df_clean_clients.iterrows():
-#    cursor.execute("INSERT INTO Clientes (Id, Nome, Email, Data_cadastro, Telefone, DDD, Country_code) values(?,?,?)", row.Id, row.Nome, row.Email, row.Data_cadastro, row.Telefone, row.DDD, row.Country_code)
+df_transaction['DataHora'] = pd.to_datetime(df_transaction['DataHora'], format='%Y-%m-%d %H:%M:%S')
+df_transaction['DataHora'] = df_transaction['DataHora'].dt.tz_localize(None)
 
- # Aplicar a função em cada linha do DataFrame usando foreach()
-# df_clean_clients.foreach(lambda row: inserir_pessoa(row))
+df_transaction = df_transaction.drop('Id', axis=1)
+
+print(df_clients.info())
+print(df_transaction.info())
+
+df_clients.to_sql('Cliente', engine, if_exists='append', index=False)
+df_transaction.to_sql('Transacao', engine, if_exists='append', index=False)
